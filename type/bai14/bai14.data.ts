8e0f267d0b3e67cbcfbbf187f93525b4a42c96eb
DucAN <162082971+DucAn333@users.noreply.github.com>
2026-08-20 11:02:46 +0700

    Add random generation for birth date and email

--- type/bai14/bai14.data.ts
+++ type/bai14/bai14.data.ts
@@ -35,11 +35,28 @@ function generateRandomPhoneNumber(): string {
     const randomChars = Array.from({ length: 7 }, () => nums[Math.floor(Math.random() * nums.length)]).join('');
     return `090${randomChars}`;
 }
+function randomBirthYear(): number {
+  const min = 1990;
+  const max = 2008;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomMonth(): number {
+  return Math.floor(Math.random() * 12) + 1;
+}
+// random ngày từ ngày 1-27, để chắc chắn khớp với tất cả các tháng
+function randomDayOfMonth(): number {
+  const min = 1;
+  const max = 27;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 const randomEmail = generateRandomEmail();
 const randomName = generateRandomName();
 const randomPassword = generateRandomPassword();
 const lastName = generateRandomLastName();
 const phone = generateRandomPhoneNumber();
+const year = randomBirthYear();
+const month =  randomMonth():
+const days = randomDayOfMonth():
 
 
 //====data config====
@@ -56,9 +73,9 @@ export const account = {
     name:randomName,
     email:randomEmail,
     password:randomPassword,
-    day:'3',
-    month:'3',
-    year:'2000',
+    day: days,
+    month: month,
+    year: year,
     firtName:randomName,
     lastName:lastName,
     address:'Manchester',
