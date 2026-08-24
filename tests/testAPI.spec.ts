2c5a03727ef92f3eeb90e5fc364bcfd47edc74fe
Nguyen Duc An <nguyenducan03.tm@gmail.com>
2026-08-24 20:19:34 +0700

    1

---
+++ tests/testAPI.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('test API', async ({ request }) => {
+ 
+    const API_URL = 'https://automationexercise.com/api/productsList'
+
+    const newRequest = await request.get(API_URL);
+    const listBrand = await newRequest.json();
+    const countBrand = listBrand.products;
+   
+    let count = 0;
+    
+    for (const brand of countBrand){
+        if (brand.brand === 'Madame'){
+            count++;
+        }
+    }
+
+    console.log(`co ${count} san pham madame  ` );
+});
