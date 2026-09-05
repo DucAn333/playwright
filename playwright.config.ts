be3789bca95faa05fd336be48187588545a92d38
Nguyen Duc An <nguyenducan03.tm@gmail.com>
2026-09-05 14:37:00 +0700

    cònig

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,6 +35,9 @@ export default defineConfig({
     viewport: null,
     headless: false,
 
+  trace: 'retain-on-failure', // Chỉ lưu Trace khi test bị Fail
+  screenshot: 'only-on-failure',
+
   },
 
   /* Configure projects for major browsers */
