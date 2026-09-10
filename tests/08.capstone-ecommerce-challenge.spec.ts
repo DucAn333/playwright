13f2aa790a0008b298f27c98d6ba6ee024847af0
DucAN <162082971+DucAn333@users.noreply.github.com>
2026-09-10 14:16:10 +0700

    sua lai

--- tests/08.capstone-ecommerce-challenge.spec.ts
+++ tests/08.capstone-ecommerce-challenge.spec.ts
@@ -114,9 +114,7 @@ test.describe("Lab 8 - Capstone Challenge: E-Commerce Flow Verification", () => 
     //   await ClearBtn.nth(i).click();
     // }
     while (Count > 0) {
-      await Clearbtn.first().click();
-      Const --;
-      
+      await ClearBtn.first().click();
     }
     //await page.waitForTimeout(500);
     // dung vong lap de chac chan click het button "x", sau do them promise de expect
