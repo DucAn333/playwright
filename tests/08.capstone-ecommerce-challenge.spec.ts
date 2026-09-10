c73d500adcffd4dde93b5aa560572ad7a0bc398a
DucAN <162082971+DucAn333@users.noreply.github.com>
2026-09-10 09:24:05 +0700

    Fix indentation and formatting in test case

--- tests/08.capstone-ecommerce-challenge.spec.ts
+++ tests/08.capstone-ecommerce-challenge.spec.ts
@@ -114,8 +114,8 @@ test.describe("Lab 8 - Capstone Challenge: E-Commerce Flow Verification", () => 
     //   await ClearBtn.nth(i).click();
     // }
     while (Count > 0) {
-    await Clearbtn.first().click();
-    await expect(Clearbtn).toHaveCount(Count - 1);
+      await Clearbtn.first().click();
+      await expect(Clearbtn).toHaveCount(Count - 1);
     }
     //await page.waitForTimeout(500);
     // dung vong lap de chac chan click het button "x", sau do them promise de expect
